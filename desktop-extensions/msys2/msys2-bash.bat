68f4979f65e6fd08e940bfeca73a7f756a58637d
damon-kwok <damon-kwok@outlook.com>
2020-03-19 18:38:37 +0800

    ext:windows

--- desktop-extensions/msys2/msys2-bash.bat
+++ desktop-extensions/msys2/msys2-bash.bat
@@ -18,5 +18,6 @@ rem )
 rem pause
 
 set PATH=D:\\msys64\\mingw64\\bin;D:\\msys64\\usr\\bin;%PATH%
+%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvars64.bat"
 cd %1
 bash
